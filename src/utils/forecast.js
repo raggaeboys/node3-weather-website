3bdc348cdfa747b79fd32c4923eed88e638d800d
Hugh Clarke <hclarke@isecurefl.com>
2020-11-11 15:54:02 -0500

    Add more details to weather details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search. ', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress . It feels like ' + body.current.feelslike + ' degrees' + ' and the humidity is ' +body.current.humidity+ ' degree ')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress . It feels like ' + body.current.feelslike + ' degrees' + ' and the humidity is '  +body.current.humidity + ' degree. '+ ' The local time is '+  body.current.observation_time +  'Time of day is ' + body.current.is)
             // (undefined, body.location.timezone_id)
         }
 
